1b002333cadb1d14ea721fd8792aecbbe4001d1c
Anvy Tran <anvytran857@gmail.com>
2021-04-02 10:41:29 -0400

    updated owner page with win/losses

--- app/routes.js
+++ app/routes.js
@@ -8,30 +8,7 @@ module.exports = function(app, passport, db) {
     });
 
     // PROFILE SECTION =========================
-    // app.get('/profile', isLoggedIn, function(req, res) {
-    //     db.collection('profits').find().toArray((err, resultMain) => {
-    //       db.collection('profits').aggregate(
-    //         [
-    //           {$match: {// match acts as a filter to find all of the documents with the conditions below
-    //             winner: 'casino',//these are said conditions, in this case we are filtering all docs where winner is the casino
-    //             }
-    //           },
-    //           {$group: {_id: null,// group puts all of the returned matches in one group
-    //                     total: { $sum: '$betAmount'}}// this code sets the total property to the sum of all of the bet amounts in the group
-    //           }
-    //         ]).toArray().then((result) => {
-    //           console.log(result)
-    //           if (err) return console.log(err)
-    //           let total = result[0].total
-    //           console.log(total);
-    //           res.render("profile.ejs", {
-    //             resultMain: profits,
-    //             profit: total,
-    //             user: req.user
-    //       })
-    //       })
-    //     })
-    // });
+
 
     app.get('/profile', function(req, res) {
 
@@ -59,11 +36,14 @@ module.exports = function(app, passport, db) {
           }}
         ]
       ).toArray()
+
+      let findWinnerCasino = db.collection('profits').countDocuments({winner: 'casino'})
+      let findWinnerPlayer = db.collection('profits').countDocuments({winner: 'player'})
   
-      Promise.all([findAllDocuments, findAddProfits, findLosses]).then((values) => {
+      Promise.all([findAllDocuments, findAddProfits, findLosses, findWinnerCasino, findWinnerPlayer]).then((values) => {
         
         
-        let [findAllDocuments, findAddProfitsResults, findLossesValues] = values;
+        let [findAllDocuments, findAddProfitsResults, findLossesValues, findWinnerCasinoResults, findWinnerPlayerResults] = values;
 
         console.log('Hi')
         console.log(findLossesValues)
@@ -71,13 +51,17 @@ module.exports = function(app, passport, db) {
         findLossesValues = findLossesValues.length === 0 ? findLossesValues[0].total = 0 : findLossesValues[0].total
 
         console.log(findLossesValues)
+        console.log(findWinnerPlayerResults)
   
         res.render('profile.ejs', {
           user: req.user,
           documents: findAllDocuments,
           addProfits: findAddProfitsResults,
-          subtractProfits: findLossesValues
+          subtractProfits: findLossesValues,
+          winnerCasino: findWinnerCasinoResults,
+          playerCasino: findWinnerPlayerResults
         })
+        console.log(subtractProfits)
       }).catch((error) => {
         console.log(error)
       });
